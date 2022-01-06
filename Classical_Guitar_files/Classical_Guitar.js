60f6b6a5d81915bb417a715f2bd41f60b105505d
Peter Binkley <peter.binkley@ualberta.ca>
2022-01-06 10:24:46 -0700

    Initial commit: as scraped, excluding Media directory

---
+++ Classical_Guitar_files/Classical_Guitar.js
@@ -0,0 +1,51 @@
+// Created by iWeb 3.0.4 local-build-20111109
+
+function writeMovie1()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id5" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/bachprelude.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id5" type="video/quicktime" width="117" height="16"><param name="src" value="Media/bachprelude.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id5" type="video/quicktime" width="117" height="16" data="Media/bachprelude.mov"><param name="src" value="Media/bachprelude.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie2()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id6" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/Prelude%20WTC.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id6" type="video/quicktime" width="117" height="16"><param name="src" value="Media/Prelude%20WTC.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id6" type="video/quicktime" width="117" height="16" data="Media/Prelude%20WTC.mov"><param name="src" value="Media/Prelude%20WTC.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie3()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id7" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/Jesu.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id7" type="video/quicktime" width="117" height="16"><param name="src" value="Media/Jesu.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id7" type="video/quicktime" width="117" height="16" data="Media/Jesu.mov"><param name="src" value="Media/Jesu.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie4()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id8" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/el%20noy-4.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id8" type="video/quicktime" width="117" height="16"><param name="src" value="Media/el%20noy-4.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id8" type="video/quicktime" width="117" height="16" data="Media/el%20noy-4.mov"><param name="src" value="Media/el%20noy-4.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie5()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id9" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/canarios-3.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id9" type="video/quicktime" width="117" height="16"><param name="src" value="Media/canarios-3.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id9" type="video/quicktime" width="117" height="16" data="Media/canarios-3.mov"><param name="src" value="Media/canarios-3.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie6()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id10" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="117" height="16"><param name="src" value="Media/Wachet%20Auf.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id10" type="video/quicktime" width="117" height="16"><param name="src" value="Media/Wachet%20Auf.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id10" type="video/quicktime" width="117" height="16" data="Media/Wachet%20Auf.mov"><param name="src" value="Media/Wachet%20Auf.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_01.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_02.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_03.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_06.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_09.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_08.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_07.png'),IWCreateImage('Classical_Guitar_files/Creme_sidebar_frame_04.png')],null,2,1.000000,0.000000,0.000000,0.000000,0.000000,10.000000,16.000000,10.000000,20.000000,523.000000,173.000000,523.000000,173.000000,null,null,null,0.100000),stroke_2:new IWPhotoFrame([IWCreateImage('Classical_Guitar_files/Creme_frame3_01.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_02.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_03.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_06.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_09.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_08.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_07.png'),IWCreateImage('Classical_Guitar_files/Creme_frame3_04.png')],null,0,1.000000,10.000000,10.000000,9.000000,10.000000,10.000000,10.000000,9.000000,10.000000,100.000000,150.000000,100.000000,150.000000,null,null,null,0.100000),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Classical_Guitar_files/Classical_GuitarMoz.css')
+adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');fixupIECSS3Opacity('id1');fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
